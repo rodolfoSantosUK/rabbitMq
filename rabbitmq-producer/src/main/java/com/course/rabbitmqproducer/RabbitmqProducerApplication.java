7e65eeb37525bb04e6e2440cfa762ab812836394
Rodolfo Santos <santoseuro2018@gmail.com>
2020-07-10 21:25:34 -0300

    [ADD] Consumer e producer

--- rabbitmq-producer/src/main/java/com/course/rabbitmqproducer/RabbitmqProducerApplication.java
+++ rabbitmq-producer/src/main/java/com/course/rabbitmqproducer/RabbitmqProducerApplication.java
@@ -1,13 +1,25 @@
 package com.course.rabbitmqproducer;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import com.course.rabbitmqproducer.producer.HelloRabbitProducer;
 
 @SpringBootApplication
-public class RabbitmqProducerApplication {
+public class RabbitmqProducerApplication implements CommandLineRunner {
 
+	@Autowired
+	HelloRabbitProducer helloRabbitProducer;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(RabbitmqProducerApplication.class, args);
 	}
+	
+	public void run(String... args) throws Exception {
+		helloRabbitProducer.sendHelloToQueue1("Message 1 " + Math.random());
+		helloRabbitProducer.sendHelloToQueue2("Message 2 " + Math.random());
+	}
+	
 
 }
